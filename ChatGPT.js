2816ba8abbb9ac878ac708111e80118c34a06d97
Spawn9986 <scouch9986@gmail.com>
2023-07-11 11:26:30 -0500

    completed second coding challenge

--- ChatGPT.js
+++ ChatGPT.js
@@ -1,9 +1,10 @@
 //Nested Loops are helpful when working with multiple sets of data (i.e., 2 arrays, etc.), and multi-dimensional data sets (matrices).
 
-//1) Iterating over two or more arrays:
+//Nested Loops - 1) Iterating over two or more arrays:
 
 //================ Challenge: Given two arrays, find pairs of elements that sum up to a specific target value ====================
 
+/* 
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const targetSum = 7;
@@ -18,11 +19,22 @@ function findPairs(arr1, arr2, sum) {
     }
   }
   return newArray;
-}
+} 
+
+let output = findPairs(array1, array2, targetSum);
+console.log(output);
+
+Output: [ [ 1, 6 ], [ 2, 5 ], [ 3, 4 ] ]
+*/
 
 // OR push into new obj intsead of new array
 
-/* function findPairs(arr1, arr2, sum) {
+/* 
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const targetSum = 7;
+
+function findPairs(arr1, arr2, sum) {
     let newObj = {};
     let counter = 0;
     
@@ -38,18 +50,42 @@ function findPairs(arr1, arr2, sum) {
   }
 
  let output = findPairs(array1, array2, targetSum);
- console.log(output); */
+ console.log(output); 
 
- //what i learned from this or got more comfortable with: working with multi sets of data, pushing into new arrays or new objects
+ Output: 
+ { '0': [ 1, 6 ], '1': [ 2, 5 ], '2': [ 3, 4 ] }
+ */
 
-//2) Multi-dimensional data processing:
+//what i learned from this or got more comfortable with: working with multi sets of data, pushing into new arrays or new objects
+
+//Nested Loops - 2) Multi-dimensional data processing:
 
 //================ Challenge: Given a matrix, calculate the sum of all elements ===================
 
-//3) Generating permutations and combinations:
+/* const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+function calculateMatrixSum(arrayOfArrays) {
+  let sum = 0;
+  for (let i = 0; i < arrayOfArrays.length; i++) {
+    for (let j = 0; j < arrayOfArrays[i].length; j++) {
+      sum += arrayOfArrays[i][j];
+    }
+  }
+  return sum;
+}
+
+let output = calculateMatrixSum(matrix);
+console.log(output); 
+
+//Output: 45 */
+
+//Nested Loops - 3) Generating permutations and combinations:
 
 //================= Challenge: Generate all possible combinations of two lists =============================
-
 
 /* 
 //=========== Challenge: Find Prime Numbers level 5 - 6 (on 10 pt scale) ===============
