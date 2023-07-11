2a34bedf0b0dcc62f239e19d5a39cc720be889d5
Spawn9986 <scouch9986@gmail.com>
2023-07-10 19:41:11 -0500

    added some steps to ensure number and not empty array

--- ChatGPT.js
+++ ChatGPT.js
@@ -1,16 +1,29 @@
+/* 
+
+Challenge: Calculate Average
+
 let array = [1, 2, 3, 4, 88, 9, 13, 34]
  
- let calculateAverage = (arr) => {
+ let calculateAverage = (numbers) => {
+
+    if (numbers.length === 0 | numbers.some(isNaN)) {
+        return "Invalid array"
+    }
+
     let total = 0;
     let avg;
 
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i];
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
     }
    
-    avg = total/ arr.length;
+    avg = total/ numbers.length;
     return avg;
  }
 
  let output = calculateAverage(array);
- console.log(output);
+ console.log(output); 
+ 
+ //learned about/ got more comfortable with: numbers.some(isNaN)
+
+ */
