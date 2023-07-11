fc6e901b73135ccb35a1c495eb175b454173f7d1
Spawn9986 <scouch9986@gmail.com>
2023-07-11 10:50:29 -0500

    adding some coding challenges for learning nested loops

--- ChatGPT.js
+++ ChatGPT.js
@@ -1,6 +1,25 @@
+//Nested Loops are helpful when working with multiple sets of data (i.e., 2 arrays, etc.), and multi-dimensional data sets (matrices).
+
+//1) Iterating over two or more arrays:
+
+//================ Challenge: Given two arrays, find pairs of elements that sum up to a specific target value ====================
+
+//2) Multi-dimensional data processing:
+
+//================ Challenge: Given a matrix, calculate the sum of all elements ===================
+
+//3) Generating permutations and combinations:
+
+//================= Challenge: Generate all possible combinations of two lists =============================
+
+
+/* 
+//=========== Challenge: Find Prime Numbers level 5 - 6 (on 10 pt scale) ===============
+Write a function named findPrimes that takes in two numbers, start and end, and returns an array of all prime numbers between start and end, inclusive. */
+
 /* 
 
-Challenge: Calculate Average
+//============ Challenge: Calculate Average level 2 - 3 (on 10 pt scale) ===============
 
 let array = [1, 2, 3, 4, 88, 9, 13, 34]
  
